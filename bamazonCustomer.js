9a36e17b31e614a73e867c7b4fec787dd209db88
Christopher Ducey <chrisducey01@me.com>
2019-10-12 11:53:59 -0700

    Created database seed file and setup db connection in js file

---
+++ bamazonCustomer.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const env = require("dotenv").config();
+
+const connection = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    }
+)
+
+connection.connect(err=>{
+    if(err) throw err;
+    console.log("Connection established.");
+    connection.end(err=>{
+        if(err) throw err;
+        console.log("Connection closed.");
+    });
+})
