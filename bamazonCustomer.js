06556c576c0cdaa3483bbd24ea05592f2bf67c8f
Christopher Ducey <chrisducey01@me.com>
2019-10-18 20:41:21 -0700

    Refactored inquirer prompts to allow user to quit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,9 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const env = require("dotenv").config();
 
+const ITEM_LIST = ["item_id", "product_name", "price", "stock_quantity"];
+const TABLE_NAME = "products";
+
 const connection = mysql.createConnection(
     {
         host: process.env.DB_HOST,
@@ -11,6 +14,12 @@ const connection = mysql.createConnection(
         database: process.env.DB_DATABASE
     }
 )
+
+
+function start(){
+    listAllItems(connection, TABLE_NAME, ITEM_LIST, promptForItem);
+}
+
 
 function listAllItems(connection, tableName, fieldList, callback) {
     console.log(`
@@ -26,58 +35,79 @@ function listAllItems(connection, tableName, fieldList, callback) {
 }
 
 
-function purchaseItem(connection) {
+function promptForItem(connection) {
     inquirer.prompt([
         {
             message: "Enter the item_id of what you want to buy (Enter 0 to quit):",
             type: "number",
             name: "item_id_to_buy"
-        },
-        {
-            message: "How many do you want to buy?",
-            type: "number",
-            name: "purchase_quantity"
         }
     ]).then(answers => {
         if (answers.item_id_to_buy === 0) {
             endConnection(connection);
         }
         else {
-            //See if there is enough in stock to fulfill the order
-            connection.query("Select * from products where item_id = ?", answers.item_id_to_buy, (err, res) => {
-                if (err) throw err;
-                if (res.length != 1) throw err;
-
-                //If there isn't enough in stock, don't complete the purchase
-                if (res[0].stock_quantity < answers.purchase_quantity) {
-                    console.log("Sorry there aren't enough in stock to fulfill your order.");
-                    listAllItems(connection, "products", ["item_id", "product_name", "price"], purchaseItem);
-                }
-                //else complete the purchase and update the inventory level
-                else {
-                    console.log(`Ordering ${answers.purchase_quantity} of the ${res[0].product_name}.  Thank you for your purchase!`);
-
-                    let new_quantity = res[0].stock_quantity - answers.purchase_quantity;
-                    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [new_quantity, answers.item_id_to_buy],
-                        (function (price, quantity) {
-                            return function (err, rows, fields) {
-                                if (err) throw err;
-                                // console.log(`Updating inventory`);
-                                console.log(`Order successful.  Your total is ${price * quantity}`)
-                                listAllItems(connection, "products", ["item_id", "product_name", "price"], purchaseItem);
-                            }
-                        })(res[0].price, answers.purchase_quantity)
-                    );
-                }
-            })
+            purchaseItem(connection, answers.item_id_to_buy);
         }
     })
 }
 
 
-function endConnection(connection){
+function purchaseItem(connection, itemId) {
+    inquirer.prompt([
+        {
+            message: "How many do you want to buy?  (Enter 0 to quit)",
+            type: "number",
+            name: "purchase_quantity"
+        }
+    ]).then(answers => {
+        if (answers.purchase_quantity === 0) {
+            endConnection(connection);
+        }
+        else {
+            //See if there is enough in stock to fulfill the order
+            connection.query("Select * from products where item_id = ?", itemId, (err, res) => {
+                if (err) throw err;
+                if (res.length != 1) throw err;
+
+                //If there isn't enough in stock, don't complete the purchase
+                if (res[0].stock_quantity < answers.purchase_quantity) {
+                    console.log("");
+                    console.log("Sorry there aren't enough in stock to fulfill your order.");
+                    start();
+                }
+                //else complete the purchase and update the inventory level
+                else {
+                    console.log("");
+                    console.log(`Ordering ${answers.purchase_quantity} of the ${res[0].product_name}.  Thank you for your purchase!`);
+
+                    let new_quantity = res[0].stock_quantity - answers.purchase_quantity;
+                    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [new_quantity, itemId],
+                        (function (price, quantity) {
+                            return function (err, rows, fields) {
+                                if (err) throw err;
+                                // console.log(`Updating inventory`);
+                                if (rows.changedRows != 1) {
+                                    console.log(`Sorry there was an issue placing your order.  Please try again.`);
+                                }
+                                else {
+                                    console.log(`Order successful.  Your total is ${(price * quantity).toFixed(2)}`)
+                                }
+                                start();
+                            }
+                        })(res[0].price, answers.purchase_quantity)
+                    );
+                }
+            });
+        }
+    });
+};
+
+
+function endConnection(connection) {
     connection.end(err => {
         if (err) throw err;
+        console.log("");
         console.log("Connection closed.");
 
     });
@@ -87,5 +117,6 @@ function endConnection(connection){
 connection.connect(err => {
     if (err) throw err;
     console.log("Connection established.");
-    listAllItems(connection, "products", ["item_id", "product_name", "price"], purchaseItem);
-})
+    start();
+});
+
--- db/db_seed.sql
+++ db/db_seed.sql
@@ -1,22 +1,10 @@
-DROP DATABASE IF EXISTS bamazon;
-
-CREATE DATABASE bamazon;
-
-USE bamazon;
-
-CREATE TABLE products(
-    item_id INT NOT NULL auto_increment,
-    product_name VARCHAR(100) NOT NULL,
-    department_name VARCHAR(50) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    stock_quantity INT NOT NULL,
-    PRIMARY KEY(item_id)
-);
-
+use bamazon;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
 ('Teletubbies','Toys',4.99, 3000),
-('Parks and Rec DVD''s','Electronics',79.99, 100)
+('Parks and Rec DVD''s','Electronics',79.99, 100),
+('Cards Against Humanity','Games',7.99, 30),
+('Mystery Box','Household',29.99,1)
 ;
 
