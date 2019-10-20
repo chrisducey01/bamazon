3e79b8e96378589284d19df18c67b426dd69586b
Christopher Ducey <chrisducey01@me.com>
2019-10-20 11:17:13 -0700

    Removed unused functions at bottom of code

--- bamazonManager.js
+++ bamazonManager.js
@@ -125,6 +125,7 @@ function updateInventory(connection, itemId, callback){
     });
 }
 
+
 function addNewProduct(connection, callback){
     inquirer.prompt([
         {
@@ -188,78 +189,3 @@ connection.connect(err => {
     start();
 });
 
-
-
-
-/*
-  Old code, delete before wrapping up
-
-function promptForItem(connection) {
-    inquirer.prompt([
-        {
-            message: "Enter the item_id of what you want to buy (Enter 0 to quit):",
-            type: "number",
-            name: "item_id_to_buy"
-        }
-    ]).then(answers => {
-        if (answers.item_id_to_buy === 0) {
-            endConnection(connection);
-        }
-        else {
-            purchaseItem(connection, answers.item_id_to_buy);
-        }
-    })
-}
-
-
-function purchaseItem(connection, itemId) {
-    inquirer.prompt([
-        {
-            message: "How many do you want to buy?  (Enter 0 to quit)",
-            type: "number",
-            name: "purchase_quantity"
-        }
-    ]).then(answers => {
-        if (answers.purchase_quantity === 0) {
-            endConnection(connection);
-        }
-        else {
-            //See if there is enough in stock to fulfill the order
-            connection.query("Select * from products where item_id = ?", itemId, (err, res) => {
-                if (err) throw err;
-                if (res.length != 1) throw err;
-
-                //If there isn't enough in stock, don't complete the purchase
-                if (res[0].stock_quantity < answers.purchase_quantity) {
-                    console.log("");
-                    console.log("Sorry there aren't enough in stock to fulfill your order.");
-                    start();
-                }
-                //else complete the purchase and update the inventory level
-                else {
-                    console.log("");
-                    console.log(`Ordering ${answers.purchase_quantity} of the ${res[0].product_name}.  Thank you for your purchase!`);
-
-                    let new_quantity = res[0].stock_quantity - answers.purchase_quantity;
-                    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [new_quantity, itemId],
-                        (function (price, quantity) {
-                            return function (err, rows, fields) {
-                                if (err) throw err;
-                                // console.log(`Updating inventory`);
-                                if (rows.changedRows != 1) {
-                                    console.log(`Sorry there was an issue placing your order.  Please try again.`);
-                                }
-                                else {
-                                    console.log(`Order successful.  Your total is ${(price * quantity).toFixed(2)}`)
-                                }
-                                start();
-                            }
-                        })(res[0].price, answers.purchase_quantity)
-                    );
-                }
-            });
-        }
-    });
-};
-
-*/
